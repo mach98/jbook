eef90ca773081abd26ebaacbd02eee9c1260842b
Williams B <dbolade2000@gmail.com>
2022-07-29 18:44:44 +0100

    last commit for a while

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
 
@@ -38,6 +38,11 @@ const App = () => {
     //console.log(result);
     setCode(result.outputFiles[0].text);
   };
+  const html = `
+    <script>
+      ${code}
+    </script>
+    `;
   return (
     <div>
       <textarea
@@ -48,6 +53,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
